d89b77af5a2501c7a787a098ca30cc17bc080a2e
Divyam <divyamsaxena123@gmail.com>
2020-07-27 16:46:04 +0530

    Added folder structure

--- controllers/home_controller.js
+++
@@ -1,9 +0,0 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('doctor_id', 25);
-    return res.render('home', {
-        title: "Hospital"
-    });
-}
-
-// module.exports.actionName = function(req, res){}
